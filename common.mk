e977f236cf0105fe1517dc0e0b27547b1fabc551
xtang@qnx.com <xtang@qnx.com>
2026-04-16 19:03:31 +0800

    Port llama.cpp B8808

--- common.mk
+++ common.mk
@@ -8,61 +8,62 @@ include $(MKFILES_ROOT)/qmacros.mk
 NAME=llama.cpp
 QNX_PROJECT_ROOT ?= $(shell readlink -f $(PROJECT_ROOT)/../../../$(NAME))
 
-#where to install:
-LLAMA_INSTALL_ROOT ?= $(INSTALL_ROOT_$(OS))
+# staging
+QNX_BASE:=$(notdir $(shell readlink -f $(QNX_HOST)/../../../))
+INSTALL_ROOT_nto := /usr/local/stage
+$(NAME)_INSTALL_ROOT ?= $(INSTALL_ROOT_nto)/$(NAME)/$(QNX_BASE)
 
 PREFIX ?= /usr/local
 
-#choose Release or Debug
-CMAKE_BUILD_TYPE ?= Debug
+CMAKE_BUILD_TYPE ?= Release
 
 #override 'all' target to bypass the default QNX build system
-ALL_DEPENDENCIES = llama_all
-.PHONY: llama_all test
+ALL_DEPENDENCIES = $(NAME)_all
+.PHONY: $(ANME)_all install clean test
 
-CFLAGS += $(FLAGS)
-
+CFLAGS += $(FLAGS) -D_QNX_SOURCE -O3 -fPIC 
+CFLAGS += -I$(INSTALL_ROOT_$(OS))/$(PREFIX)/include -I$(INSTALL_ROOT_$(OS))/$(CPUVARDIR)/$(PREFIX)/include
+CXXFLAGS += $(CFLAGS)
+LDFLAGS += -lgomp -lsocket -lc++
 include $(MKFILES_ROOT)/qtargets.mk
 
 #CMake env
-CMAKE_MODULE_PATH := $(QNX_TARGET)/$(CPUVARDIR)/$(PREFIX)/lib/cmake;$(INSTALL_ROOT)/$(CPUVARDIR)/$(PREFIX)/lib/cmake
-
-CFLAGS += -I$(INSTALL_ROOT)/$(PREFIX)/include 
-LDFLAGS += -lgomp -lsocket -lc++
+#CMAKE_FIND_ROOT_PATH := $(QNX_TARGET);$(QNX_TARGET)/$(CPUVARDIR);$(INSTALL_ROOT_$(OS))/$(CPUVARDIR)
+#CMAKE_MODULE_PATH := $(QNX_TARGET)/$(CPUVARDIR)/$(PREFIX)/lib/cmake;$(INSTALL_ROOT_$(OS))/$(CPUVARDIR)/$(PREFIX)/lib/cmake
 
 CMAKE_ARGS = -DCMAKE_TOOLCHAIN_FILE=$(PROJECT_ROOT)/qnx.nto.toolchain.cmake \
              -DCMAKE_SYSTEM_PROCESSOR=$(CPUVARDIR) \
              -DCPU=${CPU} \
-             -DCMAKE_INSTALL_PREFIX=$(LLAMA_INSTALL_ROOT)/$(CPUVARDIR)/$(PREFIX) \
-             -DCMAKE_INSTALL_INCLUDEDIR=$(LLAMA_INSTALL_ROOT)/$(PREFIX)/include \
+             -DCMAKE_INSTALL_PREFIX=$($(NAME)_INSTALL_ROOT)/$(CPUVARDIR)/$(PREFIX) \
+             -DCMAKE_INSTALL_INCLUDEDIR=$($(NAME)_INSTALL_ROOT)/$(PREFIX)/include \
              -DCMAKE_MODULE_PATH="$(CMAKE_MODULE_PATH)" \
              -DCMAKE_FIND_ROOT_PATH="$(CMAKE_FIND_ROOT_PATH)" \
              -DCMAKE_BUILD_TYPE=$(CMAKE_BUILD_TYPE) \
              -DEXTRA_CMAKE_C_FLAGS="$(CFLAGS)" \
-             -DEXTRA_CMAKE_CXX_FLAGS="$(CFLAGS)" \
+             -DEXTRA_CMAKE_CXX_FLAGS="$(CXXFLAGS)" \
              -DEXTRA_CMAKE_LINKER_FLAGS="$(LDFLAGS)" \
-             -DGCC_VER=${GCC_VER}
+             -DGCC_VER=${GCC_VER} 
 
 MAKE_ARGS ?= -j $(firstword $(JLEVEL) 4)
+BUILDDIR ?= build_$(QNX_BASE)
 
-ifndef NO_TARGET_OVERRIDE
 include $(PROJECT_ROOT)/patch.mk
 
-llama_all: apply-patch
-	@mkdir -p build
-	cd build && cmake $(CMAKE_ARGS) $(QNX_PROJECT_ROOT)
-	cd build && make all $(MAKE_ARGS)
+CMAKE_DEBUG =
 
 
-install: llama_all
-	@cd build && make $(MAKE_ARGS) install 
+$(NAME)_all: apply-patch
+	@mkdir -p $(BUILDDIR) 
+	cd $(BUILDDIR) && cmake $(CMAKE_DEBUG) $(CMAKE_ARGS) $(QNX_PROJECT_ROOT)
+	cd $(BUILDDIR) && make all $(MAKE_ARGS)
 
-clean iclean spotless: clean-patch
-	rm -fr build
+TARGET_INSTALL=@cd $(BUILDDIR) && make $(MAKE_ARGS) install 
+EXTRA_ICLEAN=$(BUILDDIR)
 
-cuninstall uninstall:
+clean_all: clean-patch 
+	rm -rf $(BUILDDIR)
+	rm -rf $($(NAME)_INSTALL_ROOT) 
 
 test: 
 	cd build && $(PROJECT_ROOT)/ctest2cmd.sh $(PROJECT_ROOT) $(QNX_PROJECT_ROOT)
 
-endif
